94386a3d1be65505317cfdc12f4fe82a16df6a66
Leonardo Benicio <5027245+lbenicio@users.noreply.github.com>
2024-05-10 17:02:05 -0300

    add service unit test init file

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,27 +16,27 @@ describe('AppController', () => {
 
   describe('verifyJWT', () => {
     it('should return "verdadeiro" upon submit of correct token', () => {
-      const jwt =
+      const token =
         'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJTZWVkIjoiNzg0MSIsIk5hbWUiOiJUb25pbmhvIEFyYXVqbyJ9.QY05sIjtrcJnP533kQNk8QXcaleJ1Q01jWY_ZzIZuAg';
-      expect(appController.verifyJWT(jwt)).toBe('verdadeiro');
+      expect(appController.verifyJWT(token)).toBe('verdadeiro');
     });
 
     it('should return "falso" upon submit of invalid token', () => {
-      const jwt =
+      const token =
         'eyJhbGciOiJzI1NiJ9.dfsdfsfryJSr2xrIjoiQWRtaW4iLCJTZrkIjoiNzg0MSIsIk5hbrUiOiJUb25pbmhvIEFyYXVqbyJ9.QY05fsdfsIjtrcJnP533kQNk8QXcaleJ1Q01jWY_ZzIZuAg';
-      expect(appController.verifyJWT(jwt)).toBe('falso');
+      expect(appController.verifyJWT(token)).toBe('falso');
     });
 
     it('should return "falso" upon submit of invalid claim name, containing numbers', () => {
-      const jwt =
+      const token =
         'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiRXh0ZXJuYWwiLCJTZWVkIjoiODgwMzciLCJOYW1lIjoiTTRyaWEgT2xpdmlhIn0.6YD73XWZYQSSMDf6H0i3-kylz1-TY_Yt6h1cV2Ku-Qs';
-      expect(appController.verifyJWT(jwt)).toBe('falso');
+      expect(appController.verifyJWT(token)).toBe('falso');
     });
 
     it('should return "falso" upon submit of invalid token, containing more then 3 claims', () => {
-      const jwt =
+      const token =
         'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiTWVtYmVyIiwiT3JnIjoiQlIiLCJTZWVkIjoiMTQ2MjciLCJOYW1lIjoiVmFsZGlyIEFyYW5oYSJ9.cmrXV_Flm5mfdpfNUVopY_I2zeJUy4EZ4i3Fea98zvY';
-      expect(appController.verifyJWT(jwt)).toBe('falso');
+      expect(appController.verifyJWT(token)).toBe('falso');
     });
   });
 });
