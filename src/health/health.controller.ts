c3fa31950159ab9e1fadbb6c43a93cf11be00dc2
Leonardo Benicio <5027245+lbenicio@users.noreply.github.com>
2024-05-10 19:17:47 -0300

    add dependencies + lint

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,24 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import {
-    HealthCheckService,
-    HttpHealthIndicator,
-    HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  HealthCheck,
 } from '@nestjs/terminus';
-import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
-    constructor(
-        private readonly health: HealthCheckService,
-        private readonly http: HttpHealthIndicator,
-    ) {}
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly http: HttpHealthIndicator,
+  ) {}
 
-    @Get()
-    @Public()
-    @HealthCheck()
-    check() {
-        return this.health.check([
-            () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
-        ]);
-    }
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+    ]);
+  }
 }
